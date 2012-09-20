efea75e998d592e8c8a1eff6a2d61030e387f3d8
Vladimir Brasil <vladimir.brasil@gmail.com>
2012-09-20 16:32:57 -0300

    Add user microposts

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -16,6 +16,32 @@ describe "Static pages" do
 
     it_should_behave_like "all static pages"
     it { should_not have_selector 'title', text: '| Home' }
+
+    describe "for signed-in users" do
+      before do
+        DatabaseCleaner.strategy = :transaction
+        DatabaseCleaner.clean_with(:truncation)
+      end
+      before  { DatabaseCleaner.start }     
+      after   { DatabaseCleaner.clean }     
+
+      let(:user) { FactoryGirl.create(:user) }
+      before do
+        100.times { FactoryGirl.create(:micropost) }
+        # FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")
+        # FactoryGirl.create(:micropost, user: user, content: "Dolor sit amet")
+        sign_in user
+        visit root_path
+      end
+
+      it "should render the user's feed" do
+#        user.feed.each do |item|
+        user.feed.paginate(page: 1).each do |item|
+          page.should have_selector("li##{item.id}", text: item.content)
+        end
+      end
+
+    end
   end
 
   describe "Help page" do
